6f5a8b15f69213b767fff1623b8c85df67b57141
abubakar055 <abubakarnawaz055@gmail.com>
2024-06-04 21:44:42 +0500

    integrated the material UI

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,6 @@
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../theme';
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -11,7 +14,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>
+            {children}
+          </ThemeProvider>
+        </AppRouterCacheProvider>
+        </body>
     </html>
   );
 }
